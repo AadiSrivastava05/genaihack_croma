dc0e7e9d75549c3585ae80447bd11cc6186ef326
Achintya Jai <153343775+pUrGe12@users.noreply.github.com>
2024-10-03 12:59:52 +0530

    Update CommLandSupaScrape.py

--- scrape/CommLandSupaScrape.py
+++ scrape/CommLandSupaScrape.py
@@ -1,37 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
 import re
-
 from geopy.geocoders import Nominatim
 from geopy.distance import geodesic
 import pandas as pd
 import geopandas as gpd
-
 import time
-
 import os
 from supabase import create_client, Client
 from requests.structures import CaseInsensitiveDict
 import urllib.parse
 
-'''
-url = "https://www.magicbricks.com/srp_commercial_land1.xml"
 
-headers = {
-    "User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Mobile Safari/537.36"
-}
+url: str = ""
+key: str = ""
 
-webpage = requests.get(url, headers=headers).text
-
-with open("commercial_land_data.txt", "w") as fp:
- 	fp.write(webpage)
-
-'''
-
-url: str = "https://ajkgqdvxmueqxtuvcjho.supabase.co"
-key: str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFqa2dxZHZ4bXVlcXh0dXZjamhvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU1NTYyOTcsImV4cCI6MjA0MTEzMjI5N30.cwmfECvEYqu6FZXRFqcTw1zD-H6NoHkG_etfqPZ-hDw"
-
-apiKey = '085e718f0205455db5cac133ebf11b34'
+apiKey = ''
 
 headers1 = CaseInsensitiveDict()
 headers1["Accept"] = "application/json"
@@ -76,10 +60,9 @@ for url in links[i:]:
             mainInfo['bboxTRightLat'] = bboxa[-1]
             mainInfo['bboxTRightlng'] = bboxa[-2]
             
-            response = supabase.table('CommercialLandCoords').insert(mainInfo).execute()
+            response = supabase.table('').insert(mainInfo).execute()
             print(mainInfo, i)
     
-    # Handle timeout exception
     except requests.exceptions.Timeout:
         print(f"Request timed out for URL: {url.strip()}, moving to next link.")
     
